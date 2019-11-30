5d8e9d8ae23c13c50c70c8859a6a0325960af6d8
Luigi-Crisci <luigicrisci1997@gmail.com>
2019-11-30 20:47:40 +0100

    a

--- Application/src/main/java/myapp/StorageController.java
+++ Application/src/main/java/myapp/StorageController.java
@@ -1,5 +1,9 @@
 package myapp;
 
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties.Env;
+
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
 import com.microsoft.azure.AzureEnvironment;
 import com.microsoft.azure.credentials.AppServiceMSICredentials;
 import com.microsoft.azure.management.Azure;
@@ -10,12 +14,16 @@ public class StorageController {
 	private String subscriptionId="9ffa1034-e692-4bb6-aaa7-172fa6352018";
 	
 	public void connect() {
-		Azure azure = Azure.authenticate(new AppServiceMSICredentials(AzureEnvironment.AZURE))
-		        .withSubscription(subscriptionId);
-		
-		StorageAccount account= azure.storageAccounts().getByResourceGroup("azureias-rg", "azureiasstorage");
-		System.out.println("Account id: " + account.id());
+//		Azure azure = Azure.authenticate(new AppServiceMSICredentials(AzureEnvironment.AZURE))
+//		        .withSubscription(subscriptionId);
+//		StorageAccount account= azure.storageAccounts().getByResourceGroup("azureias-rg", "azureiasstorage");
+//		System.out.println("Account id: " + account.id());
 		//Vault myKeyVault = azure.vaults().getByResourceGroup(resourceGroup, keyvaultName);
+		
+		String str=System.getenv("CONNECT_STR");
+		BlobServiceClient client= new BlobServiceClientBuilder().connectionString(str).buildClient();
+		client.createBlobContainer("TestJavaContainer");
+		
 	
 	}
 
